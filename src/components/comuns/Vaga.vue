3417c7e1032679a444a3732ea7e0f3ba96f144d7
Otavio Silva Junior <otavio.srds1@gmail.com>
2022-08-26 21:09:48 -0300

    Otavio - alterações realizadas nos componentes!

--- src/components/comuns/Vaga.vue
+++ src/components/comuns/Vaga.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="card">
-    <div class="card-header bg-dark text-white">Título da vaga</div>
-    <div class="card-body"><p>Descrição tuncada da vaga...</p></div>
+    <div class="card-header bg-dark text-white">{{titulo}}</div>
+    <div class="card-body"><p>{{descricao}}</p></div>
     <div class="card-footer">
         <small class="text-muted"
-        >Salário: R$ 6.000,00 | Modalidade: Home Office | Tipo: CLT |
-        Publicação: 20/08/2022
+        >Salário: {{salario}} | Modalidade: {{modalidade}} | Tipo: {{tipo}} |
+        Publicação: {{publicacao}}
         </small>
     </div>
   </div>
@@ -14,7 +14,8 @@
 <script>
 /* eslint-disable */
 export default {
-    name: 'Vaga'
+    name: 'Vaga',
+    props: ['titulo','descricao','salario', 'modalidade', 'tipo', 'publicacao']
 }
 /* eslint-disable */
 </script>
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -5,12 +5,9 @@
 
         <pesquisar-vaga></pesquisar-vaga>
 
-        <div class="row mt-5">
+        <div class="row mt-5" v-for="(vaga, index) in vagas" :key="index">
           <div class="col-6">
-            <vaga></vaga>
-          </div>
-          <div class="col-6">
-            <vaga></vaga>
+            <vaga v-bind="vaga"></vaga>
           </div>
         </div>
 
@@ -47,6 +44,48 @@ export default {
   data() {
     return {
       usuariosOnline: 0,
+      vagas: [
+        {
+          titulo: 'Analista Programador PHP Pleno',
+          descricao: 'Profissional com conhecimentos em PHP, Laravel e MySQL. Necessário 3 anos de experiências. Atuará na manutenção de sistemas legados da empresa.',
+          salario: 6000,
+          modalidade: 'Home Office',
+          tipo: 'PJ',
+          publicacao: '2021-10-10'
+        },
+        {
+          titulo: 'Programador JavaScript Angular',
+          descricao: 'Profissional com conhecimentos avançados em JavaScript e Angular.',
+          salario: 5000,
+          modalidade: 'Presencial',
+          tipo: 'CLT',
+          publicacao: '2021-10-07'
+        },
+        {
+          titulo: 'Programador JavaScript Vue',
+          descricao: 'Profissional com conhecimentos avançados em JavaScript e Vue.',
+          salario: 5000,
+          modalidade: 'Home Office',
+          tipo: 'CLT',
+          publicacao: '2021-10-06'
+        },
+        {
+          titulo: 'Analista de Banco de Dados Sênior',
+          descricao: 'Domínio dos bancos de dados SQL Server, Oracle, Postgre e MySQL',
+          salario: 9000,
+          modalidade: 'Presencial',
+          tipo: 'PJ',
+          publicacao: '2021-10-06'
+        },
+        {
+          titulo: 'Programador Web Júnior',
+          descricao: 'Conhecimentos básicos em HTML, CSS, JavaScript, Bootstrap, PHP e MySQL',
+          salario: 3000,
+          modalidade: 'Presencial',
+          tipo: 'CLT',
+          publicacao: '2021-10-05'
+        }
+      ]
     }
   },
   methods: {
